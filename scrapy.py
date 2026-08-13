8c3b864599218465daf44e4443a099da314237aa
yaom65806 <yaom65806@gmail.com>
2026-08-13 11:34:22 +0800

    Update scrapy.py to 25

--- scrapy.py
+++ scrapy.py
@@ -45,7 +45,7 @@ def get_All_Sessions(Area_With_Date):
 
 #Sort all the pdf file link
 def sort_PDF():
-    TopicURL = get_All_Sessions(Area_With_Date="US-26")
+    TopicURL = get_All_Sessions(Area_With_Date="US-25")
     All_PDF=[]
     for url in TopicURL:
         kill_child_processes(os.getpid())
